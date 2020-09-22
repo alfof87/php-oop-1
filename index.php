2e1442d85787928ed138ffdaca76d836419fc65e
Alfonso <alfof87@gmail.com>
2020-09-22 15:53:40 +0200

    php-oop-1

--- index.php
+++ index.php
@@ -5,13 +5,13 @@
     public $email;
     public $avatar;
 
-    public function _construct($username, $password, $email, $avatar){
+    public function __construct($username, $password, $email, $avatar){
       $this -> username = $username;
       $this -> password = $password;
       $this -> email = $email;
       $this -> avatar = $avatar;
     }
-    public function _toString(){
+    public function __toString(){
       return "Username: " . $this -> username . "<br>"
             . "Password: " . $this -> password . "<br>"
             . "email: " . $this -> email . "<br>"
@@ -20,7 +20,7 @@
   }
 
   $u1 = new User("pippo", "pluto", "pippo@mail.com", "x");
-  $u2 = new User("paperino", "rossi", "pap@mail.com", "x");
+  $u2 = new User("paperino", "rossi", "pap@mail.com", ?><img src="https://lh3.googleusercontent.com/proxy/X-8yhOtrra3GbJU7fN7j5poCHMv-MR2DmAQCMc5VTn-f4PLKLHwhOLqzeuuu_hIH5xzodIE2R-T9HTvTgqj0_MAN2nfH_y8IPitKsObT508906Bi-tZlLq7wyYzfwylXPnZg273D6Cwivg1WdkCT_4PO" alt="">  <?php);
 
   echo $u1;
   echo $u2;
