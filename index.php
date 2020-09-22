7e1d4d05bfde5a88b9bb91837d6ceefa110eb10f
Alfonso <alfof87@gmail.com>
2020-09-22 17:18:54 +0200

    php-oop-1

--- index.php
+++ index.php
@@ -27,9 +27,9 @@
   $img1 = '<img src="https://upload-icon.s3.us-east-2.amazonaws.com/uploads/icons/png/4664092721557740377-512.png" style="width:50px;">';
   $img2 = '<img src="https://upload-icon.s3.us-east-2.amazonaws.com/uploads/icons/png/5202224201557740378-512.png" style="width:50px;">';
 
+  $psw1 = rand ( void ): int
 
-
-  $u1 = new User("pippo", "pluto", "pippo@mail.com", "$img1");
+  $u1 = new User("pippo", "$psw1", "pippo@mail.com", "$img1");
   $u2 = new User("paperino", "rossi", "pap@mail.com", "$img2");
 
   echo $u1;
